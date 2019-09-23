97a52540f44e09b2c629d5f992323963c5c65723
ruben.vazquez <Ruben.Vazquez@dieboldnixdorf.com>
2019-09-23 17:19:06 -0500

    first commit

---
+++ layouts/default.vue
@@ -0,0 +1,90 @@
+<template>
+  <div>
+    <nav
+      class="navbar header has-shadow is-primary"
+      role="navigation"
+      aria-label="main navigation"
+    >
+      <div class="navbar-brand">
+        <a class="navbar-item" href="/">Super Películas</a>
+
+        <div class="navbar-burger" @click="isActive = !isActive">
+          <span />
+          <span />
+          <span />
+        </div>
+      </div>
+
+      <!-- Contenido del menú, al nivel del navbar-brand-->
+      <div class="navbar-menu" :class="{active:isActive}">
+        <div class="navbar-end">
+          <div class="navbar-item">
+            <div class="buttons">
+              <a class="button">
+                <strong>Registro</strong>
+              </a>
+              <a class="button is-light">
+                Acceder
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+
+
+    </nav>
+
+    <section class="main-content columns">
+      <aside class="column is-2 section">
+        <p class="menu-label is-hidden-touch">
+          General
+        </p>
+        <ul class="menu-list">
+          <li
+            v-for="(item, key) of items"
+            :key="key"
+          >
+            <nuxt-link
+              :to="item.to"
+              exact-active-class="is-active"
+            >
+              <b-icon :icon="item.icon" /> {{ item.title }}
+            </nuxt-link>
+          </li>
+        </ul>
+      </aside>
+
+      <div class="container column is-10">
+        <nuxt />
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      isActive: true,
+      items: [
+        {
+          title: 'peliculas',
+          icon: 'home',
+          to: { name: 'index' }
+        },
+        {
+          title: 'Favoritas',
+          icon: 'lightbulb',
+          to: { name: 'favoritas' }
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+.active{
+  display: block !important;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,54 @@
+<template>
+  <section class="section">
+    <h2 class="title">Películas</h2>
+    <div class="columns is-mobile is-multiline">
+      <Peli v-for="peli in pelis" :key="peli.id" :peli="peli" />
+    </div>
+  </section>
+</template>
+<script>
+import Peli from "~/components/Peli";
+
+export default {
+  components: {
+    Peli
+  },
+
+  data: function() {
+    return {
+      pelis: [
+        {
+          id: 1,
+          titulo: "El Padrino",
+          favoritos: "3",
+          imagen:
+            "https://m.media-amazon.com/images/M/MV5BM2MyNjYxNmUtYTAwNi00MTYxLWJmNWYtYzZlODY3ZTk3OTFlXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_UY268_CR3,0,182,268_AL_.jpg",
+          estreno: "1972",
+          resumen:
+            "The aging patriarch of an organized crime dynasty transfers control of his clandestine empire to his reluctant son."
+        },
+        {
+          id: 2,
+          titulo: "El Padrino: Parte 2",
+          favoritos: "1",
+          imagen:
+            "https://m.media-amazon.com/images/M/MV5BMWMwMGQzZTItY2JlNC00OWZiLWIyMDctNDk2ZDQ2YjRjMWQ0XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_UY268_CR3,0,182,268_AL_.jpg",
+          estreno: "1974",
+          resumen:
+            "The early life and career of Vito Corleone in 1920s New York City is portrayed, while his son, Michael, expands and tightens his grip on the family crime syndicate."
+        },
+        {
+          id: 3,
+          titulo: "El Padrino: Parte 3",
+          favoritos: "0",
+          imagen:
+            "https://m.media-amazon.com/images/M/MV5BNTc1YjhiNzktMjEyNS00YmNhLWExYjItZDhkNWJjZjYxOWZiXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_UX182_CR0,0,182,268_AL_.jpg",
+          estreno: "1990",
+          resumen:
+            "In the midst of trying to legitimize his business dealings in New York City and Italy in 1979, aging Mafia Don Michael Corleone seeks to avow for his sins, while taking his nephew Vincent Mancini under his wing."
+        }
+      ]
+    };
+  }
+};
+</script>
